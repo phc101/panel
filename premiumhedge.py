f42409eb004881d97c38cc8411936470b08ca5f0
phc101 <tomek@phc.com.pl>
2025-02-13 13:21:18 +0100

    premiumhedge.py

--- premiumhedge.py
+++ premiumhedge.py
@@ -1,7 +1,7 @@
 from serpapi import GoogleSearch
 
 params = {
-    "api_key": api_key,
+    "api_key": e9626f65ccba8349ed8f9a3b9fbb448092d151f7d8998df5a8bc4c354c85e31a,
     "engine": "google_finance",
     "q": "EURPLN"
 }
