9cdf269db5b611b4cf6890b11047cdfa716e36c6
phc101 <tomek@phc.com.pl>
2025-02-13 13:38:54 +0100

    premiumhedge.py

--- premiumhedge.py
+++ premiumhedge.py
@@ -3,11 +3,11 @@ import requests
 # Your SerpAPI Key
 api_key = "e9626f65ccba8349ed8f9a3b9fbb448092d151f7d8998df5a8bc4c354c85e31a"
 
-# Test URL for EUR/PLN
-url = f"https://serpapi.com/search?api_key={api_key}&engine=google_finance&q=EURPLN"
+# Define the SerpAPI endpoint for Google Finance
+url = f"https://serpapi.com/search?engine=google_finance&q=EURPLN&api_key={api_key}"
 
-# Fetch data
+# Send request
 response = requests.get(url)
 
-# Print response
+# Print raw response
 print(response.json())
