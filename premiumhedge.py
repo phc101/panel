bc60a820e36eafc9ec7883bb0cf8736aa10157f3
phc101 <tomek@phc.com.pl>
2025-03-08 17:14:00 +0100

    premiumhedge.py

--- premiumhedge.py
+++ premiumhedge.py
@@ -78,7 +78,9 @@ def main():
         
         # Plot Cumulative Revenue
         fig, ax = plt.subplots()
-        ax.plot(result_df["Entry Date"], result_df["Cumulative Revenue %"], marker='o', linestyle='-')
+        colors = ['green' if x > 0 else 'red' for x in result_df["Revenue %"]]
+        ax.scatter(result_df["Entry Date"], result_df["Cumulative Revenue %"], c=colors, marker='o')
+        ax.plot(result_df["Entry Date"], result_df["Cumulative Revenue %"], linestyle='-', linewidth=1), linewidth=1)
         ax.set_title("Cumulative Revenue Over Time")
         ax.set_xlabel("Date")
         ax.set_ylabel("Cumulative Revenue %")
@@ -86,7 +88,7 @@ def main():
         
         # Plot Negative Drawdown
         fig, ax = plt.subplots()
-        ax.plot(result_df["Entry Date"], -result_df["Drawdown %"], color='red', linestyle='-', label="Negative Drawdown")
+        ax.plot(result_df["Entry Date"], -result_df["Drawdown %"], color='red', linestyle='-', linewidth=1, label="Negative Drawdown")
         ax.set_title("Negative Drawdown Over Time")
         ax.set_xlabel("Date")
         ax.set_ylabel("Drawdown %")
