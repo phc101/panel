d490d12a3a7caf839c60ef4eebffb5260ecf323a
phc101 <tomek@phc.com.pl>
2025-06-30 10:42:15 +0200

    fx_hedge_planner_app.py

--- fx_hedge_planner_app.py
+++ fx_hedge_planner_app.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import requests
 from datetime import datetime, timedelta
-import yfinance as yf
 
 # Page config
 st.set_page_config(
